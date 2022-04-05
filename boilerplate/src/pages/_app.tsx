93492f4b76b5b02d1947d9675f8a29afe9e017cc
hokyunR <hokyun.lee@avikus.ai>
2022-04-05 14:58:44 +0900

    refactor: run MSW on development

--- boilerplate/src/pages/_app.tsx
+++ boilerplate/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
-if (process.env.NEXT_PUBLIC_ENABLE_API_MOCKING === "true") {
+if (process.env.NODE_ENV === "development") {
   if (typeof window !== "undefined") {
     const { worker } = require("../mocks/browser")
     worker.start()
